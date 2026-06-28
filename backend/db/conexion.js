546c3f051bd4f19229f1b7b90053c112007e3c7c
Gabriel <gabrielangl31@gmail.com>
2026-06-28 00:17:38 -0500

    Agregar SSL para conexión en producción

--- backend/db/conexion.js
+++ backend/db/conexion.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  ssl: process.env.DB_HOST === 'localhost' ? false : { rejectUnauthorized: false },
 });
 
 module.exports = pool;
